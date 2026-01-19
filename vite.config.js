e39cf7a1f272fdc0a6264b9ad08b56e20d3d2bd4
devrohaan <bagwer@tcd.ie>
2026-01-19 07:04:30 +0000

    Update Base Path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: "/devrohaan.github.io/",
+  base: "/",
   plugins: [react(), svgr()],
   resolve: {
     dedupe: ["react", "react-dom"],
